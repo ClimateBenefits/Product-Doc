1391a9b103f2bf65942d222570a497ebc18ad5b9
Parakh Jaggi <Parakh_Jaggi@baylor.edu>
2021-08-22 17:39:45 -0500

    fux url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   title: 'Climate Benefits Doc',
   tagline: 'Climate Benefits',
   url: 'https://climatebenefits.github.io/',
-  baseUrl: '/Product-Doc/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
