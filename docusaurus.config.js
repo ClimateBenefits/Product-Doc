c3e3fe58975dc5fac311d73a4341e781970d3767
Parakh Jaggi <Parakh_Jaggi@baylor.edu>
2021-08-31 20:34:30 -0500

    no more splash screen

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,14 +45,8 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
-        },
+        
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
